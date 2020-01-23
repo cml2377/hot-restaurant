23ad0ca5385dc2d864a93477a49a655568c4a3ce
Crystal Ly <crystal.m.ly@gmail.com>
2020-01-23 14:04:12 -0600

    Co-authored-by: Kurt LaVacque <lavacquek@icloud.com> Co-authored-by: Leo <leolopez10@gmail.com> Co-authored-by: Brianna McCray <52844772+bwmccray@users.noreply.github.com>

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Grabbing html files 
+// app.use(express.static('./html/home.html'))
+app.use(express.static('assets'))
+
 // Star Wars Characters (DATA)
 // =============================================================
 var tables = [{
