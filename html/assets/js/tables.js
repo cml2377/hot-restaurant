de7d031cfa21a391b3940cb7dd74066e2b2ab42a
Crystal Ly <crystal.m.ly@gmail.com>
2020-01-23 11:44:45 -0600

    Co-authored-by: Brianna McCray <52844772+bwmccray@users.noreply.github.com> Co-authored-by: Kurt LaVacque <lavacquek@icloud.com>

---
+++ html/assets/js/tables.js
@@ -0,0 +1,3 @@
+const home_b_e = $("#home_b")
+const make_reservations_b_e = $("#make_reservations_b")
+// const view_tables_b_e = $("#view_tables_b")
--- server.js
+++ server.js
@@ -13,3 +13,48 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Star Wars Characters (DATA)
+// =============================================================
+var tables = [
+    {
+        routeName: "table_1",
+        name: "Trent",
+        phone_number: "555-5555",
+        id: 1,
+    },
+    {
+        routeName: "table_2",
+        name: "Brent",
+        phone_number: "666-6666",
+        id: 1,
+    },
+    {
+        routeName: "table_3",
+        name: "Grent",
+        phone_number: "777-7777",
+        id: 1,
+    },
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
