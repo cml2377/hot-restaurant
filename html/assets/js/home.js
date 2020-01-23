1a145a27e885ac4450acc973736906881cf6213d
Crystal Ly <crystal.m.ly@gmail.com>
2020-01-23 12:05:37 -0600

    Co-authored-by: Kurt LaVacque <lavacquek@icloud.com> Co-authored-by: Brianna McCray <52844772+bwmccray@users.noreply.github.com> Co-authored-by: Leo <leolopez10@gmail.com>

--- html/assets/js/home.js
+++ html/assets/js/home.js
@@ -2,6 +2,12 @@
 const make_reservations_b_e = $("#make_reservations_b")
 const view_tables_b_e = $("#view_tables_b")
 
+const name_i_e = $("#name_i")
+const phone_number_i_e = $("#phone_number_i")
+const email_i_e = $("#email_i")
+const id_i_e = $("#id_i")
+
+
 
 make_reservations_b_e.on("click", function () {
     // var searchedCharacter = $("#character-search").val().trim();
@@ -10,18 +16,5 @@ make_reservations_b_e.on("click", function () {
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     // searchedCharacter = searchedCharacter.replace(/\s+/g, "").toLowerCase();
 
-    $.get("/api/characters/" + searchedCharacter, function (data) {
-        console.log(data);
-        if (data) {
-            $("#stats").show();
-            $("#name").text(data.name);
-            $("#role").text(data.role);
-            $("#age").text(data.age);
-            $("#force-points").text(data.forcePoints);
-        } else {
-            $("#name").text(
-                "The force is not strong with this one. Your character was not found.");
-            $("#stats").hide();
-        }
-    });
+
 });
--- html/assets/js/reserve.js
+++ html/assets/js/reserve.js
@@ -1,2 +1,35 @@
+const submit_b_e = $("#submit_b")
+
+const name_i_e = $("#name_i")
+const phone_number_i_e = $("#phone_number_i")
+const email_i_e = $("#email_i")
+const id_i_e = $("#id_i")
 
 
+submit_b_e.on("submit", function () {
+    const name = name_i_e.val()
+    console.log(name)
+    const phone_number = phone_number_i_e.val()
+    console.log(phone_number)
+    const email = email_i_e.val()
+    console.log(email)
+    const id = id_i_e.val()
+    console.log(id)
+
+}
+
+
+    // $.get("/api/characters/" + searchedCharacter, function (data) {
+    //     console.log(data);
+    //     if (data) {
+    //         $("#stats").show();
+    //         name_i_e.text(data.name);
+    //         phone_number_i_e.text(data.role);
+    //         email_i_e.text(data.age);
+    //         id_i_e.text(data.forcePoints);
+    //     } else {
+    //         $("#name").text(
+    //             "The force is not strong with this one. Your character was not found.");
+    //         $("#stats").hide();
+    //     }
+    // });
