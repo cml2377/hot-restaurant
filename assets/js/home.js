17ada2c456918c7fcaebecf43ec8b631031f4e9e
Crystal Ly <crystal.m.ly@gmail.com>
2020-01-23 19:45:39 -0600

    resolved all conflicts

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,3 +1,7 @@
+// const home_b_e = $("#home_b")
+const make_reservations_b_e = $("#make_reservations_b")
+const view_tables_b_e = $("#view_tables_b")
+
 const name_i_e = $("#name_i")
 const phone_number_i_e = $("#phone_number_i")
 const email_i_e = $("#email_i")
@@ -5,13 +9,12 @@ const id_i_e = $("#id_i")
 
 // Don't worry about me Leo, just writing JS for images in home.html
 // MDB Lightbox Init
-$(function () {
+$(functionn() {
     $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
 });
 
-// make_reservations_b_e.on("click", function () {
-
-//     $.get("/reserve", function (data) {
-//         console.log("reserve page")
-//     });
-// });
+make_reservations_b_e.on("click", functiono() {
+    $.get("/reserve", functionc(data) {
+        console.log("reserve page")
+    });
+});
--- assets/js/tables.js
+++ assets/js/tables.js
@@ -5,20 +5,20 @@
 //     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
 //     searchedCharacter = searchedCharacter.replace(/\s+/g, "").toLowerCase();
 
-$.get("/api/tables", function(data) {
+$.get("/api/tables", function (data) {
     console.log(data);
     const name = data.routeName
     console.log(name)
-//   if (data) {
-//     $("#stats").show();
-//     $("#name").text(data.name);
-//     $("#role").text(data.role);
-//     $("#age").text(data.age);
-//     $("#force-points").text(data.forcePoints);
-//   } else {
-//     $("#name").text(
-//       "The force is not strong with this one. Your character was not found.");
-//     $("#stats").hide();
-//   }
+    //   if (data) {
+    //     $("#stats").show();
+    //     $("#name").text(data.name);
+    //     $("#role").text(data.role);
+    //     $("#age").text(data.age);
+    //     $("#force-points").text(data.forcePoints);
+    //   } else {
+    //     $("#name").text(
+    //       "The force is not strong with this one. Your character was not found.");
+    //     $("#stats").hide();
+    //   }
 });
 
