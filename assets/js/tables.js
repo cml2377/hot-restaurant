c160910505a1261f50b93bccac8016c1873d51a9
Kurt LaVacque <lavacquek@icloud.com>
2020-01-24 10:32:00 -0600

    Got the table information into the html

--- assets/js/tables.js
+++ assets/js/tables.js
@@ -5,20 +5,42 @@
 //     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
 //     searchedCharacter = searchedCharacter.replace(/\s+/g, "").toLowerCase();
 
+
 $.get("/api/tables", function (data) {
-    console.log(data);
-    const name = data.routeName
-    console.log(name)
-    //   if (data) {
-    //     $("#stats").show();
-    //     $("#name").text(data.name);
-    //     $("#role").text(data.role);
-    //     $("#age").text(data.age);
-    //     $("#force-points").text(data.forcePoints);
-    //   } else {
-    //     $("#name").text(
-    //       "The force is not strong with this one. Your character was not found.");
-    //     $("#stats").hide();
-    //   }
+    for (let i = 0; i < data.length; i++) {
+        const name = data[i].name
+        const phone_number = data[i].phone_number
+        const email = data[i].name
+        const id = data[i].id
+        const table_div = $("<div>")
+        const table_container = $("#tables_container")
+        const table_h2_name = $("<h2>")
+        const table_h2_email = $("<h2>")
+        const table_h2_phone_number = $("<h2>")
+        const table_h2_id = $("<h2>")
+        const table_span_name = $("<span>")
+        const table_span_email = $("<span>")
+        const table_span_phone_number = $("<span>")
+        const table_span_id = $("<span>")
+        table_div.attr("style", "padding: 20px;")
+        table_span_name.attr("class", "label label-primary")
+        table_span_email.attr("class", "label label-primary")
+        table_span_phone_number.attr("class", "label label-primary")
+        table_span_id.attr("class", "label label-primary")
+        table_h2_name.text(`Name: ${name}`)
+        table_h2_email.text(`Email: ${email}`)
+        table_h2_phone_number.text(`Phone Number: ${phone_number}`)
+        table_h2_id.text(`id: ${id}`)
+        table_div.append(table_h2_name, table_h2_email, table_h2_phone_number, table_h2_id)
+        table_h2_name.append(table_span_name)
+        table_h2_email.append(table_span_email)
+        table_h2_phone_number.append(table_span_phone_number)
+        table_h2_id.append(table_span_id)
+        table_container.append(table_div)
+        
+    }
+    
+ 
 });
 
+const print = x => console.log(x)
